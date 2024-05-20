5e777b3b96b33373bfff318c5cdb9d51dd262170
jivesh <jsoperatorz@gmail.com>
2024-05-20 09:33:27 +0530

    eslint + prettier + typescript setup

---
+++ src/app.tsx
@@ -0,0 +1,13 @@
+import { FC } from 'react'
+
+const App: FC = () => {
+  return (
+    <div
+      className="main"
+    >
+      <h1 className="">Hello World</h1>
+    </div>
+  )
+}
+
+export default App
